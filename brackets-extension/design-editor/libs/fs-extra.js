bb27a1f04fde3c90aac341ba2d5fa54498620ce0
Kornelia Kobiela <k.kobiela@samsung.com>
2019-05-17 13:42:36 +0200

    Fix WATT issues for preview
    Issue: #49
    Problem:
    	1. Our filesystem didn't have method for delete file
    	2. Paths schema is different on both platform
    Solution: Add necessery methods and fix bugs
    
    Signed-off-by: Kornelia Kobiela <k.kobiela@samsung.com>

--- brackets-extension/design-editor/libs/fs-extra.js
+++ brackets-extension/design-editor/libs/fs-extra.js
@@ -1,71 +1,83 @@
-var FileSystem = window.top.brackets.getModule('filesystem/FileSystem'),
-    ProjectManager = window.top.brackets.getModule('project/ProjectManager');
+const FileSystem = window.top.brackets.getModule('filesystem/FileSystem'),
+	ProjectManager = window.top.brackets.getModule('project/ProjectManager');
 
 function readFile(name, encoding, callback) {
-    var file = FileSystem.getFileForPath(name);
-    file.read({}, callback);
+	const file = FileSystem.getFileForPath(name);
+	file.read({}, callback);
 }
 
 function refresh(callback) {
-    var args = arguments;
-    ProjectManager.refreshFileTree();
-    if (callback) {
-        callback.call(args);
-    }
+	const args = arguments;
+	ProjectManager.refreshFileTree();
+	if (callback) {
+		callback.call(args);
+	}
 }
 
 function existsDir(name, callback) {
-    var file = FileSystem.getDirectoryForPath(name);
-    file.exists(callback);
+	const file = FileSystem.getDirectoryForPath(name);
+	file.exists(callback);
 }
 
 function makeDir(dirName, callback) {
-    var dir = FileSystem.getDirectoryForPath(dirName);
-    dir.create(callback);
+	const dir = FileSystem.getDirectoryForPath(dirName);
+	dir.create(callback);
 }
 
 function writeFile(name, data, callback) {
-    var file = FileSystem.getFileForPath(name),
-        dirName = name.replace(/[^/]+$/, '');
+	const file = FileSystem.getFileForPath(name),
+		dirName = name.replace(/[^/]+$/, '');
 
-    existsDir(dirName, function (notUsed, isDirectory) {
-        if (isDirectory) {
-            file.write(data, {}, refresh.bind(null, callback));
-        } else {
-            makeDir(dirName, function () {
-                file.write(data, {}, refresh.bind(null, callback));
-            });
-        }
-    });
+	existsDir(dirName, (notUsed, isDirectory) => {
+		if (isDirectory) {
+			file.write(data, {}, refresh.bind(null, callback));
+		} else {
+			makeDir(dirName, () => {
+				file.write(data, {}, refresh.bind(null, callback));
+			});
+		}
+	});
 }
 
 function exists(name, callback) {
-    var file = FileSystem.getFileForPath(name);
-    file.exists((err, _exists) => {
-        callback(_exists);
-    });
+	const file = FileSystem.getFileForPath(name);
+	file.exists((err, _exists) => {
+		callback(_exists);
+	});
 }
 
 function copy(src, dest, callback) {
-    FileSystem.getFileForPath(src).read({}, function (error, content) {
-        FileSystem.getFileForPath(dest).write(content, {}, refresh.bind(null, callback));
-    });
+	FileSystem.getFileForPath(src).read({}, (error, content) => {
+		FileSystem
+			.getFileForPath(dest)
+			.write(content, {}, refresh.bind(null, callback));
+	});
 }
 
 function readDir(name, callback) {
-    var dir = FileSystem.getDirectoryForPath(name);
-    dir.getContents((err, directories) => {
-        const names = directories.map(_dir => _dir.name);
-        callback(err, names);
-    });
+	const dir = FileSystem.getDirectoryForPath(name);
+	dir.getContents((err, directories) => {
+		const names = directories.map(_dir => _dir.name);
+		callback(err, names);
+	});
+}
+
+function deleteFile(name, callback) {
+	ProjectManager.deleteItem(
+		FileSystem.getFileForPath(name)
+	);
+	if (callback) {
+		callback();
+	}
 }
 
 module.exports = {
-    readFile: readFile,
-    writeFile: writeFile,
-    makeDir: makeDir,
-    existsDir: existsDir,
-    exists: exists,
-    copy: copy,
-    readdir: readDir
+	readFile: readFile,
+	writeFile: writeFile,
+	makeDir: makeDir,
+	existsDir: existsDir,
+	exists: exists,
+	copy: copy,
+	readdir: readDir,
+	deleteFile
 };
