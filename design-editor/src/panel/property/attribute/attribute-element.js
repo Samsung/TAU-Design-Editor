72e462428f60ab447c306902f45e8b600d5ab588
Kornelia Kobiela <k.kobiela@samsung.com>
2019-05-24 13:12:36 +0200

    Enable image visibility in Design Editor
    ISSUE: #59
    PROBLEM: Some of the callings of global objects were written as
    window.top
    
    SOLUTION: Change it to utils.checkGlobalContext() which allow us to get
    the value from window in VSC build and from window.top in WATT build
    Signed-off-by: Kornelia Kobiela <k.kobiela@samsung.com>

--- design-editor/src/panel/property/attribute/attribute-element.js
+++ design-editor/src/panel/property/attribute/attribute-element.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 'use babel';
 
 import fs from 'fs';
@@ -526,11 +527,11 @@ class Attribute extends DressElement {
             const reader = new FileReader();
             reader.addEventListener('loadend', event => {
                 if (event.target.readyState === FileReader.DONE) {
-                    const dir = window.top.globalData.fileUrl.replace(/[^\/]+$/gi, '');
+                    const dir = utils.checkGlobalContext('globalData').fileUrl.replace(/[^\/]+$/gi, '');
                     const writepath = (dir + '/images/' + file.name).replace(/\/+/gi, '/');
                     const readpath = ('images/' + file.name).replace(/\/+/gi, '/');
 
-                    window.top.writeFile(
+                    utils.checkGlobalContext('writeFile')(
                         writepath,
                         event.target.result,
                         {
