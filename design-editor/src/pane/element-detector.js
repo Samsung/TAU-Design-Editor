ef4edb8109654c8b4757c9d7936bfa31b74b3915
Hubert Siwkin <h.siwkin@samsung.com>
2019-05-27 17:34:04 +0200

    Refactor webpack config files
    [Issue] N/A
    [Problem] ESLint errors in webpack files.
    [Solution] I've moved common functionality to util file for webpack.
    
    Signed-off-by: Hubert Siwkin <h.siwkin@samsung.com>

--- design-editor/src/pane/element-detector.js
+++ design-editor/src/pane/element-detector.js
@@ -7,79 +7,85 @@ let _instance = null;
 const INTERNAL_ID_ATTRIBUTE = 'data-id';
 
 function isElementElementSelectable($element, componentPackage, filter) {
-    return $element.length && $element.attr(INTERNAL_ID_ATTRIBUTE) && ElementDetector._isSelectable(componentPackage, filter);
+	return (
+		$element.length &&
+        $element.attr(INTERNAL_ID_ATTRIBUTE) &&
+        ElementDetector._isSelectable(componentPackage, filter)
+	);
 }
 
 class ElementDetector {
-    /**
-     * Constructor
+
+	constructor() {
+		this._componentPackages = packageManager.getPackages(Package.TYPE.COMPONENT);
+	}
+
+	/**
+     * Return instance (it's a singleton)
+     * @returns {ElementDetector}
      */
-    constructor() {
-        this._componentPackages = packageManager.getPackages(Package.TYPE.COMPONENT);
-    }
+	static getInstance() {
+		if (_instance === null) {
+			_instance = new ElementDetector();
+		}
 
-    /**
-     * Return instance
-     * @returns {*}
-     */
-    static getInstance() {
-        if (_instance === null) {
-            _instance = new ElementDetector();
-        }
+		return _instance;
+	}
 
-        return _instance;
-    }
-
-    /**
+	/**
      * Return true if given element is selectable
      * @param {Object} pack
      * @param {String} filter
      * @returns {boolean}
      * @protected
      */
-    static _isSelectable(pack, filter) {
-        return pack && (!filter || filter === Package.TYPE.COMPONENT || pack.options.type === filter);
-    }
+	static _isSelectable(pack, filter) {
+		return pack && (
+			!filter || filter === Package.TYPE.COMPONENT || filter === pack.options.type
+		);
+	}
 
-    /**
+	/**
      * find selectable element from argument to body and return that element.
      * @param {HTMLElement} element
      * @param {String} filter
      * @returns {Object | null}
      */
-    detect(element, filter) {
-        var $currentTarget = $(element),
-            $baseElement = null,
-            baseElement = null,
-            matchedPackage = null,
-            found = false;
+	detect(element, filter) {
+		let $currentTarget = $(element),
+			$baseElement = null,
+			baseElement = null,
+			matchedPackage = null,
+			found = false;
 
-        if ($currentTarget.length && !$currentTarget.is('.lock, .lock *')) {
-            do {
-                matchedPackage = this._componentPackages.getPackageByElement($currentTarget);
-                if (isElementElementSelectable($currentTarget, matchedPackage, filter)) {
-                    found = true;
-                    break;
-                } else if (matchedPackage && matchedPackage.options.baseElementSelector) {
-                    $baseElement = $currentTarget.find(matchedPackage.options.baseElementSelector);
-                    if (isElementElementSelectable($baseElement, matchedPackage, filter)) {
-                        baseElement = $baseElement.get(0);
-                        // copy internal ID on wrapper element
-                        $currentTarget.attr(INTERNAL_ID_ATTRIBUTE, $baseElement.attr(INTERNAL_ID_ATTRIBUTE));
-                        found = true;
-                        break;
-                    }
-                }
-            } while (found === false && ($currentTarget = $currentTarget.parent()) && $currentTarget.length);
+		if ($currentTarget.length && !$currentTarget.is('.lock, .lock *')) {
 
-        }
-        return found ? {
-            $element: $currentTarget,
-            package: matchedPackage,
-            element: $currentTarget[0],
-            baseElement: baseElement
-        } : null;
-    }
+			while (!found && $currentTarget && $currentTarget.length) {
+				matchedPackage = this._componentPackages.getPackageByElement($currentTarget);
+				if (isElementElementSelectable($currentTarget, matchedPackage, filter)) {
+					found = true;
+				} else if (matchedPackage && matchedPackage.options.baseElementSelector) {
+					$baseElement = $currentTarget.find(matchedPackage.options.baseElementSelector);
+
+					if (isElementElementSelectable($baseElement, matchedPackage, filter)) {
+						baseElement = $baseElement.get(0);
+						// copy internal ID on wrapper element
+						$currentTarget.attr(INTERNAL_ID_ATTRIBUTE, $baseElement.attr(INTERNAL_ID_ATTRIBUTE));
+						found = true;
+					}
+				}
+				$currentTarget = $currentTarget.parent();
+			}
+
+		}
+
+		return found ? {
+			$element: $currentTarget,
+			package: matchedPackage,
+			element: $currentTarget[0],
+			baseElement: baseElement
+		} : null;
+	}
 }
 
 export {ElementDetector};
--- tools/build.watt.config.js
+++ tools/build.watt.config.js
@@ -1,179 +1,198 @@
 /* eslint-env mode, es6 */
 /* global module, __dirname */
 const path = require('path');
-const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-const NormalModuleReplacementPlugin = webpack.NormalModuleReplacementPlugin;
 const CONTEXT = path.resolve(__dirname, '..');
+const {createAliasObject} = require('./webpack-utils')(CONTEXT);
 const OUTPUT = path.resolve(CONTEXT, 'dist/watt');
 const plugins = [
-    new CopyWebpackPlugin([
-        {from: path.resolve(CONTEXT, 'brackets-extension')},
-        {from: path.resolve(CONTEXT, 'design-editor/src'), to: path.resolve(OUTPUT, 'design-editor/closet')},
-        {from: path.resolve(CONTEXT, 'closet-default-component-packages'), to: path.resolve(OUTPUT, 'design-editor/node_modules/closet-default-component-packages')},
-        {from: path.resolve(CONTEXT, 'closet-component-packages'), to: path.resolve(OUTPUT, 'design-editor/node_modules/closet-component-packages')},
-        {from: path.resolve(CONTEXT, 'tau-component-packages'), to: path.resolve(OUTPUT, 'design-editor/node_modules/tau-component-packages')},
-        {from: path.resolve(CONTEXT, 'content-manager'), to: path.resolve(OUTPUT, 'design-editor/node_modules/content-manager')},
-        {from: path.resolve(CONTEXT, 'design-editor/styles'), to: path.resolve(OUTPUT, 'design-editor/styles')}
-    ])
+	new CopyWebpackPlugin([
+		{
+			from: path.resolve(CONTEXT, 'brackets-extension')
+		},
+		{
+			from: path.resolve(CONTEXT, 'design-editor/src'),
+			to: path.resolve(OUTPUT, 'design-editor/closet')
+		},
+		{
+			from: path.resolve(CONTEXT, 'closet-default-component-packages'),
+			to: path.resolve(OUTPUT, 'design-editor/node_modules/closet-default-component-packages')
+		},
+		{
+			from: path.resolve(CONTEXT, 'closet-component-packages'),
+			to: path.resolve(OUTPUT, 'design-editor/node_modules/closet-component-packages')
+		},
+		{
+			from: path.resolve(CONTEXT, 'tau-component-packages'),
+			to: path.resolve(OUTPUT, 'design-editor/node_modules/tau-component-packages')
+		},
+		{
+			from: path.resolve(CONTEXT, 'content-manager'),
+			to: path.resolve(OUTPUT, 'design-editor/node_modules/content-manager')
+		},
+		{
+			from: path.resolve(CONTEXT, 'design-editor/styles'),
+			to: path.resolve(OUTPUT, 'design-editor/styles')
+		}
+	])
 ];
-const alias = {
-    labels: path.resolve(CONTEXT, 'brackets-extension/design-editor/labels.js'),
-    fs: path.resolve(CONTEXT, 'brackets-extension/design-editor/libs/fs-mock.js'),
-    'fs-extra': path.resolve(CONTEXT, 'brackets-extension/design-editor/libs/fs-extra.js'),
-    'fs-remote': path.resolve(CONTEXT, 'brackets-extension/design-editor/libs/fs-remote.js'),
-    atom: path.resolve(CONTEXT, 'brackets-extension/design-editor/libs/atom.js'),
-    remote: path.resolve(CONTEXT, 'design-editor/libs/remote.js'),
-    'closet-default-component-packages': path.resolve(CONTEXT, 'closet-default-component-packages'),
-    'closet-component-packages': path.resolve(CONTEXT, 'closet-component-packages'),
-    'tau-component-packages': path.resolve(CONTEXT, 'tau-component-packages'),
-    'content-manager': path.resolve(CONTEXT, 'content-manager'),
-    dress: path.resolve(CONTEXT, 'contents/dist/1.0.0/dress.js'),
-    mustache: path.resolve(CONTEXT, 'design-editor/node_modules/mustache/mustache.js'),
-    path: path.resolve(CONTEXT, 'design-editor/node_modules/path-browserify/index.js'),
-    'js-beautify': path.resolve(CONTEXT, 'design-editor/node_modules/js-beautify/js/index.js'),
-    jquery: path.resolve(CONTEXT, 'design-editor/node_modules/jquery/dist/jquery.js'),
-    'jquery-ui': path.resolve(CONTEXT, 'design-editor/node_modules/jquery-ui/jquery-ui.js'),
-    'assistant-view': path.resolve(CONTEXT, 'brackets-extension/design-editor/libs/assistant-view/assistant-view-manager.js'),
-    'html-assistant-editor': path.resolve(CONTEXT, 'brackets-extension/design-editor/libs/html-assistant-editor.js')
-};
+
+const relativeAliases = [
+	['labels', 'brackets-extension/design-editor/labels.js'],
+	['fs', 'brackets-extension/design-editor/libs/fs-mock.js'],
+	['fs-extra', 'brackets-extension/design-editor/libs/fs-extra.js'],
+	['fs-remote', 'brackets-extension/design-editor/libs/fs-remote.js'],
+	['atom', 'brackets-extension/design-editor/libs/atom.js'],
+	['remote', 'design-editor/libs/remote.js'],
+	['closet-default-component-packages', 'closet-default-component-packages'],
+	['closet-component-packages', 'closet-component-packages'],
+	['tau-component-packages', 'tau-component-packages'],
+	['content-manager', 'content-manager'],
+	['dress', 'contents/dist/1.0.0/dress.js'],
+	['mustache', 'design-editor/node_modules/mustache/mustache.js'],
+	['path', 'design-editor/node_modules/path-browserify/index.js'],
+	['js-beautify', 'design-editor/node_modules/js-beautify/js/index.js'],
+	['jquery', 'design-editor/node_modules/jquery/dist/jquery.js'],
+	['jquery-ui', 'design-editor/node_modules/jquery-ui/jquery-ui.js'],
+	['assistant-view', 'brackets-extension/design-editor/libs/assistant-view/assistant-view-manager.js'],
+	['html-assistant-editor', 'brackets-extension/design-editor/libs/html-assistant-editor.js']
+];
 
 const production = {
-    name: 'production',
-    context: CONTEXT,
-    node: {
-        __filename: true,
-        __dirname: true
-    },
-    entry: path.resolve(CONTEXT, 'design-editor/src/brackets/brackets-editor.js'),
-    output: {
-        path: OUTPUT,
-        filename: 'design-editor/design-editor.bundle.js'
-    },
-    target: 'web',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|css|graceful-fs)/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            plugins: [
-                                'transform-es2015-parameters',
-                                'transform-es2015-spread',
-                                'transform-es2015-arrow-functions',
-                                'transform-es2015-block-scoped-functions',
-                                'transform-es2015-block-scoping',
-                                'check-es2015-constants',
-                                'transform-es2015-destructuring',
-                                'transform-es2015-literals',
-                                'transform-es2015-shorthand-properties',
-                                'transform-es2015-template-literals'
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'css-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    {loader: 'style-loader'},
-                    {loader: 'css-loader'},
-                    {loader: 'less-loader'}
-                ]
-            }
-        ]
-    },
-    resolve: {
-        alias: alias
-    },
-    plugins: plugins
+	name: 'production',
+	context: CONTEXT,
+	node: {
+		__filename: true,
+		__dirname: true
+	},
+	entry: path.resolve(CONTEXT, 'design-editor/src/brackets/brackets-editor.js'),
+	output: {
+		path: OUTPUT,
+		filename: 'design-editor/design-editor.bundle.js'
+	},
+	target: 'web',
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|css|graceful-fs)/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							plugins: [
+								'transform-es2015-parameters',
+								'transform-es2015-spread',
+								'transform-es2015-arrow-functions',
+								'transform-es2015-block-scoped-functions',
+								'transform-es2015-block-scoping',
+								'check-es2015-constants',
+								'transform-es2015-destructuring',
+								'transform-es2015-literals',
+								'transform-es2015-shorthand-properties',
+								'transform-es2015-template-literals'
+							]
+						}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'css-loader'
+					}
+				]
+			},
+			{
+				test: /\.less$/,
+				use: [
+					{loader: 'style-loader'},
+					{loader: 'css-loader'},
+					{loader: 'less-loader'}
+				]
+			}
+		]
+	},
+	resolve: {
+		alias: createAliasObject(relativeAliases)
+	},
+	plugins: plugins
 };
 
 const development = {
-    name: 'development',
-    context: CONTEXT,
-    node: {
-        __filename: true,
-        __dirname: true
-    },
-    entry: path.resolve(CONTEXT, 'design-editor/src/brackets/brackets-editor.js'),
-    output: {
-        path: OUTPUT,
-        filename: 'design-editor/design-editor.bundle.js'
-    },
-    optimization: {
-        minimize: false,
-        namedModules: true,
-        namedChunks: true
-    },
-    target: 'web',
-    devtool: 'inline-source-map',
-    stats: 'errors-only',
-    performance: {
-        hints: false
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|css|graceful-fs)/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            plugins: [
-                                'transform-es2015-parameters',
-                                'transform-es2015-spread',
-                                'transform-es2015-arrow-functions',
-                                'transform-es2015-block-scoped-functions',
-                                'transform-es2015-block-scoping',
-                                'check-es2015-constants',
-                                'transform-es2015-destructuring',
-                                'transform-es2015-literals',
-                                'transform-es2015-shorthand-properties',
-                                'transform-es2015-template-literals'
-                            ],
-                            compact: false,
-                            comments: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'css-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    {loader: 'style-loader'},
-                    {loader: 'css-loader'},
-                    {loader: 'less-loader'}
-                ]
-            }
-        ]
-    },
-    resolve: {
-        alias: alias
-    },
-    plugins: plugins
+	name: 'development',
+	context: CONTEXT,
+	node: {
+		__filename: true,
+		__dirname: true
+	},
+	entry: path.resolve(CONTEXT, 'design-editor/src/brackets/brackets-editor.js'),
+	output: {
+		path: OUTPUT,
+		filename: 'design-editor/design-editor.bundle.js'
+	},
+	optimization: {
+		minimize: false,
+		namedModules: true,
+		namedChunks: true
+	},
+	target: 'web',
+	devtool: 'inline-source-map',
+	stats: 'errors-only',
+	performance: {
+		hints: false
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|css|graceful-fs)/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							plugins: [
+								'transform-es2015-parameters',
+								'transform-es2015-spread',
+								'transform-es2015-arrow-functions',
+								'transform-es2015-block-scoped-functions',
+								'transform-es2015-block-scoping',
+								'check-es2015-constants',
+								'transform-es2015-destructuring',
+								'transform-es2015-literals',
+								'transform-es2015-shorthand-properties',
+								'transform-es2015-template-literals'
+							],
+							compact: false,
+							comments: true
+						}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'css-loader'
+					}
+				]
+			},
+			{
+				test: /\.less$/,
+				use: [
+					{loader: 'style-loader'},
+					{loader: 'css-loader'},
+					{loader: 'less-loader'}
+				]
+			}
+		]
+	},
+	resolve: {
+		alias: createAliasObject(relativeAliases)
+	},
+	plugins: plugins
 };
 
 module.exports = [development, production];
