361dd5b96bd8665200c5dc9340433b4df76c35e9
Hubert Siwkin <h.siwkin@samsung.com>
2019-06-04 19:32:07 +0200

    Load styles restricted by media queries in DE
    [Problem] No fish-eye effect in DE Live Preview
    [Solution] I've removed "virtually" media queries for round devices
    
    Signed-off-by: Hubert Siwkin <h.siwkin@samsung.com>

--- design-editor/src/system/panel-manager.js
+++ design-editor/src/system/panel-manager.js
@@ -8,246 +8,246 @@ import {PanelElement} from './../panel/panel';
  *
  */
 class PanelManager {
-    /**
-     * Init
-     * @param {Object} itemViews
-     */
-    initialize(itemViews) {
-        var panelContainer = null,
-            containers = {},
-            closetContainer = null,
-            panelCenter = null,
-            panelMiddle = null,
-            document = itemViews && itemViews.ownerDocument;
-        // list of modal panels
-        this._panels = new WeakMap();
-        this._dimmed = false;
-        this._bindClosetEvent();
+	/**
+	 * Init
+	 * @param {Object} itemViews
+	 */
+	initialize(itemViews) {
+		var panelContainer = null,
+			containers = {},
+			closetContainer = null,
+			panelCenter = null,
+			panelMiddle = null,
+			document = itemViews && itemViews.ownerDocument;
+		// list of modal panels
+		this._panels = new WeakMap();
+		this._dimmed = false;
+		this._bindClosetEvent();
 
-        closetContainer = document.createElement('div');
-        closetContainer.className = 'closet-container';
+		closetContainer = document.createElement('div');
+		closetContainer.className = 'closet-container';
 
-        panelContainer = document.createElement('div');
-        panelContainer.className = 'closet-panel-container';
+		panelContainer = document.createElement('div');
+		panelContainer.className = 'closet-panel-container';
 
-        panelCenter = panelContainer.cloneNode();
-        panelCenter.classList.add('closet-panel-container-center');
+		panelCenter = panelContainer.cloneNode();
+		panelCenter.classList.add('closet-panel-container-center');
 
-        panelMiddle = panelContainer.cloneNode();
-        panelMiddle.classList.add('closet-panel-container-middle');
+		panelMiddle = panelContainer.cloneNode();
+		panelMiddle.classList.add('closet-panel-container-middle');
 
-        containers = {
-            top: panelContainer.cloneNode(),
-            right: panelContainer.cloneNode(),
-            left: panelContainer.cloneNode(),
-            bottom: panelContainer.cloneNode()
-        };
+		containers = {
+			top: panelContainer.cloneNode(),
+			right: panelContainer.cloneNode(),
+			left: panelContainer.cloneNode(),
+			bottom: panelContainer.cloneNode()
+		};
 
-        Object.keys(containers).forEach((name) => {
-            containers[name].classList.add('closet-panel-container-' + name);
-        });
-        closetContainer.appendChild(containers.left);
-        panelMiddle.appendChild(containers.top);
-        panelMiddle.appendChild(panelCenter);
-        panelMiddle.appendChild(containers.bottom);
-        closetContainer.appendChild(panelMiddle);
-        closetContainer.appendChild(containers.right);
-        itemViews.appendChild(closetContainer);
+		Object.keys(containers).forEach((name) => {
+			containers[name].classList.add(`closet-panel-container-${name}`);
+		});
+		closetContainer.appendChild(containers.left);
+		panelMiddle.appendChild(containers.top);
+		panelMiddle.appendChild(panelCenter);
+		panelMiddle.appendChild(containers.bottom);
+		closetContainer.appendChild(panelMiddle);
+		closetContainer.appendChild(containers.right);
+		itemViews.appendChild(closetContainer);
 
-        this.containers = containers;
-    }
+		this.containers = containers;
+	}
 
-    /**
-     * Bind closet event
-     * @private
-     */
-    _bindClosetEvent() {
-        eventEmitter.on(EVENTS.OpenPanel, this._onOpenPanel.bind(this));
-        eventEmitter.on(EVENTS.ClosePanel, this._onClosePanel.bind(this));
+	/**
+	 * Bind closet event
+	 * @private
+	 */
+	_bindClosetEvent() {
+		eventEmitter.on(EVENTS.OpenPanel, this._onOpenPanel.bind(this));
+		eventEmitter.on(EVENTS.ClosePanel, this._onClosePanel.bind(this));
 
-        eventEmitter.on(EVENTS.OpenPanel, this._onOpenPanel.bind(this));
-        eventEmitter.on(EVENTS.ClosePanel, this._onClosePanel.bind(this));
-    }
+		eventEmitter.on(EVENTS.OpenPanel, this._onOpenPanel.bind(this));
+		eventEmitter.on(EVENTS.ClosePanel, this._onClosePanel.bind(this));
+	}
 
-    /**
-     * Open panel callback
-     * @param {Object} options
-     * @private
-     */
-    _onOpenPanel(options /* type, item */) {
-        this.openPanel(options);
-    }
-    /**
-     * event handler for close displayed modal panel.
-     * if clean flag is true, modal panel will be deleted in panel list.
-     * @param {object} options
-     *      {HTMLElement} item
-     *      {boolean} clean
-     */
-    _onClosePanel(options /* item, clean */) {
-        this.closePanel(options.item);
-        if (options.clean === true) {
-            this.deletePanel(options.item);
-        }
-    }
+	/**
+	 * Open panel callback
+	 * @param {Object} options
+	 * @private
+	 */
+	_onOpenPanel(options /* type, item */) {
+		this.openPanel(options);
+	}
+	/**
+	 * event handler for close displayed modal panel.
+	 * if clean flag is true, modal panel will be deleted in panel list.
+	 * @param {object} options
+	 *      {HTMLElement} item
+	 *      {boolean} clean
+	 */
+	_onClosePanel(options /* item, clean */) {
+		this.closePanel(options.item);
+		if (options.clean === true) {
+			this.deletePanel(options.item);
+		}
+	}
 
-    /**
-     * Create panel
-     * @param {string} side
-     * @param {Object} opt
-     * @returns {*}
-     */
-    createPanel(side, opt) {
-        var options = opt,
-            panel = null,
-            container = this.containers[side];
+	/**
+	 * Create panel
+	 * @param {string} side
+	 * @param {Object} opt
+	 * @returns {*}
+	 */
+	createPanel(side, opt) {
+		var options = opt,
+			panel = null,
+			container = this.containers[side];
 
-        options = options || {};
+		options = options || {};
 
-        panel = new PanelElement();
+		panel = new PanelElement();
 
-        panel.hide();
-        if (side !== 'modal') {
-            container = this.containers[side];
-        } else {
-            container = this.containers.left.parentElement;
-            panel.classList.add('modal');
-        }
+		panel.hide();
+		if (side !== 'modal') {
+			container = this.containers[side];
+		} else {
+			container = this.containers.left.parentElement;
+			panel.classList.add('modal');
+		}
 
-        if (container === undefined) {
-            return null;
-        }
+		if (container === undefined) {
+			return null;
+		}
 
-        panel.classList.add('closet-editor-panel');
+		panel.classList.add('closet-editor-panel');
 
-        panel.style.zIndex = options.priority || 100;
+		panel.style.zIndex = options.priority || 100;
 
-        if (options.item) {
-            panel.appendChild(options.item);
-        }
+		if (options.item) {
+			panel.appendChild(options.item);
+		}
 
-        container.appendChild(panel);
+		container.appendChild(panel);
 
-        setTimeout(() => {
-            panel.show();
-        }, 0);
+		setTimeout(() => {
+			panel.show();
+		}, 0);
 
-        return panel;
-    }
+		return panel;
+	}
 
-    /**
-     * Open New Panel
-     * @param {Object} options
-     * @returns {Object}
-     */
-    openPanel(options /* type, item. visible, priority */) {
-        var panelInstance = this.getPanel(options.item);
+	/**
+	 * Open New Panel
+	 * @param {Object} options
+	 * @returns {Object}
+	 */
+	openPanel(options /* type, item. visible, priority */) {
+		var panelInstance = this.getPanel(options.item);
 
-        if (panelInstance) {
-            panelInstance.show();
-        } else {
-            if (options.type === 'modal') {
-                if (editor.isAtom()) {
-                    panelInstance = editor.workspace.addModalPanel({
-                        item: options.item,
-                        visible: options.visible || true,
-                        priority: options.priority || 100
-                    });
-                } else {
-                    panelInstance = this.createPanel(options.type, options);
-                }
-                this._dimmed = true;
-            } else {
-                panelInstance = this.createPanel(options.type, options);
-            }
-            this._panels.set(options.item, panelInstance);
-        }
-        if (options.type !== 'modal' && (options.visible || options.visible === undefined)) {
-            panelInstance.classList.add('closet-editor-panel-visible');
-        }
+		if (panelInstance) {
+			panelInstance.show();
+		} else {
+			if (options.type === 'modal') {
+				if (editor.isAtom()) {
+					panelInstance = editor.workspace.addModalPanel({
+						item: options.item,
+						visible: options.visible || true,
+						priority: options.priority || 100
+					});
+				} else {
+					panelInstance = this.createPanel(options.type, options);
+				}
+				this._dimmed = true;
+			} else {
+				panelInstance = this.createPanel(options.type, options);
+			}
+			this._panels.set(options.item, panelInstance);
+		}
+		if (options.type !== 'modal' && (options.visible || options.visible === undefined)) {
+			panelInstance.classList.add('closet-editor-panel-visible');
+		}
 
-        return panelInstance;
-    }
-    /**
-     * close panel
-     * @param {HTMLElement} panelItem
-     */
-    closePanel(panelItem) {
-        var panelInstance = this.getPanel(panelItem);
+		return panelInstance;
+	}
+	/**
+	 * close panel
+	 * @param {HTMLElement} panelItem
+	 */
+	closePanel(panelItem) {
+		var panelInstance = this.getPanel(panelItem);
 
-        if (panelInstance && !panelInstance.item) {
-            panelInstance.hide();
-            this._dimmed = false;
-        }
-    }
-    /**
-     * delete panel from panel list(weakmap)
-     * @param {HTMLElement} panelItem
-     */
-    deletePanel(panelItem) {
-        var panelInstance = this.getPanel(panelItem);
-        if (panelInstance) {
-            panelInstance.destroy();
-            this._panels.delete(panelItem);
-        }
-    }
+		if (panelInstance && !panelInstance.item) {
+			panelInstance.hide();
+			this._dimmed = false;
+		}
+	}
+	/**
+	 * delete panel from panel list(weakmap)
+	 * @param {HTMLElement} panelItem
+	 */
+	deletePanel(panelItem) {
+		var panelInstance = this.getPanel(panelItem);
+		if (panelInstance) {
+			panelInstance.destroy();
+			this._panels.delete(panelItem);
+		}
+	}
 
-    /**
-     * get panel from panel list (weakmap)
-     * @param {HTMLElement} panelItem
-     * @returns {Object}
-     */
-    getPanel(panelItem) {
-        return this._panels.get(panelItem);
-    }
+	/**
+	 * get panel from panel list (weakmap)
+	 * @param {HTMLElement} panelItem
+	 * @returns {Object}
+	 */
+	getPanel(panelItem) {
+		return this._panels.get(panelItem);
+	}
 
-    /**
-     * Return top panels
-      * @returns {*}
-     */
-    getTopPanels() {
-        return editor.workspace.getTopPanels();
-    }
+	/**
+	 * Return top panels
+	  * @returns {*}
+	 */
+	getTopPanels() {
+		return editor.workspace.getTopPanels();
+	}
 
-    /**
-     * Return bottom panels
-      * @returns {*}
-     */
-    getBottomPanels() {
-        return editor.workspace.getBottomPanels();
-    }
+	/**
+	 * Return bottom panels
+	  * @returns {*}
+	 */
+	getBottomPanels() {
+		return editor.workspace.getBottomPanels();
+	}
 
-    /**
-     * Return left panels
-      * @returns {*}
-     */
-    getLeftPanels() {
-        return editor.workspace.getLeftPanels();
-    }
+	/**
+	 * Return left panels
+	  * @returns {*}
+	 */
+	getLeftPanels() {
+		return editor.workspace.getLeftPanels();
+	}
 
-    /**
-     * Return right panels
-      * @returns {*}
-     */
-    getRightPanels() {
-        return editor.workspace.getRightPanels();
-    }
+	/**
+	 * Return right panels
+	  * @returns {*}
+	 */
+	getRightPanels() {
+		return editor.workspace.getRightPanels();
+	}
 
-    /**
-     * Return modal panels
-      * @returns {*}
-     */
-    getModalPanels() {
-        return editor.workspace.getModalPanels();
-    }
+	/**
+	 * Return modal panels
+	  * @returns {*}
+	 */
+	getModalPanels() {
+		return editor.workspace.getModalPanels();
+	}
 
-    /**
-     *
-      * @returns {boolean}
-     */
-    isDimmed() {
-        return this._dimmed;
-    }
+	/**
+	 *
+	  * @returns {boolean}
+	 */
+	isDimmed() {
+		return this._dimmed;
+	}
 }
 
 const panelManager = new PanelManager();
