69e8fadbaf4d5f8b57de3c70996550ea2e862045
Kornelia Kobiela <k.kobiela@samsung.com>
2019-08-16 10:24:46 +0200

    Enable element removing after select it indirectely
    [Issue]: #280
    [Problem]:
    	-  When select element from structure view item cannot be
    deleted
    	- When select other way than directly, focus is not set on
    closet-select-layer
    [Solution] Change focus to closet-select-layer everytime when item is
    selected
    
    Signed-off-by: Kornelia Kobiela <k.kobiela@samsung.com>

--- design-editor/src/pane/element-selector.js
+++ design-editor/src/pane/element-selector.js
@@ -1,52 +1,66 @@
+/* eslint-disable no-console */
 'use babel';
 
 import {EVENTS, eventEmitter} from '../events-emitter';
 
 class ElementSelector {
-    /**
+	/**
      * Constructor
      */
-    constructor() {
-        this._curSelectedId = null;
-    }
+	constructor() {
+		this._curSelectedId = null;
+		this._selectLayer = null;
+		eventEmitter.on(EVENTS.ActiveEditorUpdated, this.onActiveEditorUpdate);
+		eventEmitter.on(EVENTS.ElementSelected, this.onElementSelect);
+	}
 
-    /**
+	/**
      * select element
      * @param {string} elementId
      */
-    select(elementId) {
-        console.log("select", elementId);
-        if (this._curSelectedId && elementId !== this._curSelectedId) {
-            this.unSelect();
-        }
+	select(elementId) {
+		console.log('select', elementId);
+		if (this._curSelectedId && elementId !== this._curSelectedId) {
+			this.unSelect();
+		}
 
-        if (!this._curSelectedId) {
-            this._curSelectedId = elementId;
-            eventEmitter.emit(EVENTS.ElementSelected, elementId);
-        }
-    }
+		if (!this._curSelectedId) {
+			this._curSelectedId = elementId;
+			eventEmitter.emit(EVENTS.ElementSelected, elementId);
+		}
+	}
 
-    /**
+	/**
      * Unselect element
      * @returns {boolean}
      */
-    unSelect() {
-        console.log("unselect", this._curSelectedId);
-        if (this._curSelectedId) {
-            eventEmitter.emit(EVENTS.ElementDeselected, this._curSelectedId);
-            this._curSelectedId = null;
-            return true;
-        }
-        return false;
-    }
+	unSelect() {
+		console.log('unselect', this._curSelectedId);
+		if (this._curSelectedId) {
+			eventEmitter.emit(EVENTS.ElementDeselected, this._curSelectedId);
+			this._curSelectedId = null;
+			return true;
+		}
+		return false;
+	}
 
-    /**
+	/**
      * Get id of selected element
      * @returns {null|*}
      */
-    getSelectedElementId() {
-        return this._curSelectedId;
-    }
+	getSelectedElementId() {
+		return this._curSelectedId;
+	}
+
+	onActiveEditorUpdate(type, editor) {
+		if (type) {
+			this._selectLayer = editor._selectLayer.$el;
+		}
+	}
+
+	onElementSelect() {
+		this._selectLayer.focus();
+	}
 }
 
 const elementSelector = new ElementSelector();
