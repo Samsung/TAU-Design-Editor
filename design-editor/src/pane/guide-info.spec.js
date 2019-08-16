5616c8954e4f3b01c2a57637a3938750f331408e
Hubert Siwkin <h.siwkin@samsung.com>
2019-08-16 16:19:48 +0200

    Make code of guide.js more readable & maintainable
    [Issue] #262
    [Problem] Header can't be added to a new blank page
    [Solution] Fix and refactor code of guide.js (it does not fully resolve
    Issue #272)
    
    Signed-off-by: Hubert Siwkin <h.siwkin@samsung.com>

--- design-editor/src/pane/guide-info.spec.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-import {guideInfo} from './guide-info';
-
-describe('GuideInfo', function () {
-    it('should ...', function () {
-        const data = guideInfo._makeElements($('<div>'), '', {});
-        expect(true).toBe(true);
-    });
-});
--- design-editor/src/pane/interaction.spec.js
+++
@@ -1,8 +0,0 @@
-import {Interaction} from './interaction';
-
-describe('Interaction', function () {
-    it('should always return the same instance', function () {
-        const interaction = Interaction.getInstance();
-        expect(Interaction.getInstance()).toBe(interaction);
-    });
-});
