232a9a12672798eb6212b57ca4720bd681c8e097
Hubert Siwkin <h.siwkin@samsung.com>
2019-09-04 16:37:35 +0200

    Fix size reset after deselect image issue
    [Issue] https://github.com/Samsung/TAU-Design-Editor/issues/320
    [Problem] Image was shrinked down after resize & loss of focus
    [Solution] Fix the containerExpander in design-editor element (add a
    distinction if object was resized by user of by the app just for
    presentational pourposes)
    
    Signed-off-by: Hubert Siwkin <h.siwkin@samsung.com>

---
+++ design-editor/src/pane/utils/design-editor-element-utils.js
@@ -0,0 +1,39 @@
+/**
+ * It expands container to the height of
+ * sum of height of its children to make them possible
+ * to view and edit in edit mode.
+ */
+export const containerExpander = {
+	initialStyle: {},
+	element: null,
+	wasExtended: false,
+	rollOut: function(element) {
+		this._reset();
+
+		this.element = element;
+		this.initialStyle.height = element.style.height;
+		this.initialStyle.overflowY = element.style.overflowY;
+
+		const extendedHeight = [...element.children]
+			.reduce((acc, value) => acc + parseInt(getComputedStyle(value).height), 0);
+
+		if (extendedHeight > parseInt(this.initialStyle.height)) {
+			this.wasExtended = true;
+			element.style.height = `${extendedHeight}px`;
+			element.style.overflow = 'hidden';
+		}
+	},
+	rollBack: function(elementId) {
+		if (this.wasExtended && elementId == this.element.getAttribute('data-id')) {
+			this.element.style.height = this.initialStyle.height;
+			this.element.style.overflowY = this.initialStyle.overflowY;
+		}
+
+		this._reset();
+	},
+	_reset: function() {
+		this.initialStyle = {};
+		this.elementId = '';
+		this.wasExtended = false;
+	}
+};
