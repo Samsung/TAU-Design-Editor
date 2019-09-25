b14c960638468825c37ae1050c87af79ded810df
Wojciech Szczepanski <w.szczepansk@samsung.com>
2019-09-25 14:53:59 +0200

    Audio/Video Component holds its value after losing focus
    [Issue]: #365
    [Problem]: Value of audio/video component is lost when user changes focus on other element.
    [Solution]: Check value of selected component, if exists, set to input. Adding button to remove source.
    
    Signed-off-by: Wojciech Szczepanski <w.szczepansk@samsung.com>

--- closet-default-component-packages/type-elements/file/file.js
+++ closet-default-component-packages/type-elements/file/file.js
@@ -9,13 +9,11 @@ const Mustache = require('mustache'),
 	remote = require('remote'),
 	dialog = window.atom && remote.require('dialog'),
 	BrowserWindow = window.atom && remote.require('browser-window'),
-	chooseFileInput = Editor.isVSC() ?
-		'<input type="file" class="inline-block btn' +
-		' closet-attr-file-file closet-attr-long-button" ' +
-		'accept=".mp4, .mov, .flac, .aac"/>'
-		:
-		'<input type="file" class="inline-block btn' +
-		' closet-attr-file-file closet-attr-long-button" />';
+	template = `<input id="fileChoose" type="file" class="inline-block closet-attr-file-file closet-attr-long-button"
+	${Editor.isVSC() ? ' accept=".mp4, .mov, .flac, .aac"/>' : ' />'}
+	<div id="fileInputChosen" style="display:none;">
+	<input type="text" id="fileChosen" readonly="readonly" class="closet-attribute-common-textbox"/>
+	<button id="fileInputClear" class="btn">Remove</button></div>`;
 
 module.exports = dress.factory('closet-type-file', {
 
@@ -24,20 +22,47 @@ module.exports = dress.factory('closet-type-file', {
 	},
 
 	events: {
-		'click .closet-attr-file-btn': 'onClickFileBtn'
+		'click .closet-attr-file-btn': 'onClickFileBtn',
+		'click #fileInputClear': 'onClearFileSource'
 	},
 
 	onReady: function () {
 		const self = this;
-		self.$el.html(Mustache.render(chooseFileInput, self.options));
+		self.$el.html(Mustache.render(template, self.options));
 		self.$el.find('.closet-attr-file-file').on('change', event => {
 			if (event.target.files[0]['type'].match('audio.*|video.*')) {
-				AttributeUtils.writeMediaFileWhenIsLoaded(event, (filePath) => self.setValue(filePath));
+				AttributeUtils.writeMediaFileWhenIsLoaded(event, (filePath) => {
+					// set value to element and display text input with source path
+					self.setValue(filePath);
+					this.updateFileSourcePath(filePath);
+				});
 			} else {
 				// @TODO it can be used for interactive3dModel
 				self.setValue(URL.createObjectURL(event.target.files[0]));
 			}
 		});
+	},
+
+	onClearFileSource: function () {
+		this.updateFileSourcePath();
+	},
+
+	updateFileSourcePath(sourcePath) {
+		const inputFile = document.getElementById('fileChoose'),
+			inputText = document.getElementById('fileChosen'),
+			divConstrainInputText = document.getElementById('fileInputChosen');
+		if (!sourcePath) {
+			// display input type="file" because source doesn't given
+			divConstrainInputText.style.display = 'none';
+			inputFile.style.display = 'block';
+			inputFile.value = '';
+			inputFile.parentElement.setValue('');
+		} else {
+			// display input type="text" with source path
+			divConstrainInputText.style.display = 'block';
+			inputText.value = sourcePath;
+			inputFile.style.display = 'none';
+		}
 	},
 
 	onClickFileBtn: function () {
