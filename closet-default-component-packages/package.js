c108fb58dd947638c2f1510202bf698634ab4c57
Łukasz Ślachciak <l.slachciak@samsung.com>
2019-10-14 08:26:28 +0200

    Revert "Add mock of Card component"
    This reverts commit 3ee45847b9f82f6219fb891e80ffc095bc94ee1e.

--- closet-default-component-packages/package.js
+++ closet-default-component-packages/package.js
@@ -1,24 +1,23 @@
 module.exports = {
-	'name': 'closet-default-component-packages',
-	'typeElements': {
-		'image-filter': 'type-elements/image-filter',
-		'color': 'type-elements/color',
-		'effect': 'type-elements/effect',
-		'text': 'type-elements/text',
-		'checkbox': 'type-elements/checkbox',
-		'file': 'type-elements/file',
-		'select': 'type-elements/select',
-		'number': 'type-elements/number',
-		'range': 'type-elements/range',
-		'string-list': 'type-elements/string-list',
-		'select-list': 'type-elements/select-list',
-		'src': 'type-elements/src'
-	},
-	'components': {
-		'box': 'components/box',
-		'input-text': 'components/input-text',
-		'input-password': 'components/input-password',
-		'anchor': 'components/anchor',
-		'card': 'components/card'
-	}
+    'name': 'closet-default-component-packages',
+    'typeElements': {
+        'image-filter': 'type-elements/image-filter',
+        'color': 'type-elements/color',
+        'effect': 'type-elements/effect',
+        'text': 'type-elements/text',
+        'checkbox': 'type-elements/checkbox',
+        'file': 'type-elements/file',
+        'select': 'type-elements/select',
+        'number': 'type-elements/number',
+        'range': 'type-elements/range',
+        'string-list': 'type-elements/string-list',
+        'select-list': 'type-elements/select-list',
+        'src': 'type-elements/src'
+    },
+    'components': {
+        'box': 'components/box',
+        'input-text': 'components/input-text',
+        'input-password': 'components/input-password',
+        'anchor': 'components/anchor'
+    }
 };
